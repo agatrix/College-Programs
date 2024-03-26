67c9b7b24625ef72c83f476754d53170862d8ca8
Gustavo <gustavo.l.mav@gmail.com>
2024-03-26 18:30:04 -0300

    add-EscritaArq

--- TBO/Trabalho2/trabalho2.cpp
+++ TBO/Trabalho2/trabalho2.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include <cstdlib>
 #include <time.h>
+#include <fstream>
 
 #define MAX 300
 using namespace std;
@@ -25,10 +26,22 @@ public:
                 vet[j] = 1+(rand()%1000);
 
             //manda o vetor pra função
-            
+
         }
     }
 };
+
+void escritaArquivo(int numAcesso, int selecao){
+    ofstream myStream;
+    if(selecao==1)
+        myStream.open("InsertSortAcess.txt");
+    else
+        myStream.open("SelectionSortAcess.txt");
+    
+    myStream << numAcesso;
+    myStream << " ";
+    myStream.close();
+}
 
 void insertSort(int vet[], int quantidade){
     int valor, j, numAcesso=0;
@@ -49,18 +62,19 @@ void insertSort(int vet[], int quantidade){
         cout << endl;
     }
     cout << numAcesso << endl;
+    escritaArquivo(numAcesso,1);
 }
 
-void swap(int vet[], int x, int y, int numAcesso){
+void swap(int vet[], int x, int y, int *numAcesso){
     int aux = vet[x];
     vet[x] = vet[y];
     vet[y] = aux;
-    numAcesso+=3;
+    *numAcesso+=3;
 
 }
 
 
-void SelectSort(int vet[], int quantidade){
+void selectionSort(int vet[], int quantidade){
     int numAcesso = 0, atualIndex;
 
     for(int i=0; i<quantidade-1;i++){
@@ -70,13 +84,15 @@ void SelectSort(int vet[], int quantidade){
                 atualIndex = j;
         }
         if(i!=atualIndex)
-            swap(vet,i,atualIndex,numAcesso);
+            swap(vet,i,atualIndex,&numAcesso);
 
         for(int k=0; k<quantidade;k++){
             cout << vet[k] << " ";
         }
         cout << endl;
     }
+    cout << numAcesso << endl;
+    escritaArquivo(numAcesso,2);
 }
 
 
@@ -86,7 +102,7 @@ int main(){
     
     listaEst.insere(listaEst.vet);
 
-    //insertSort(listaEst.vet,listaEst.quantidade);
-    //SelectSort(listaEst.vet,listaEst.quantidade);
+    insertSort(listaEst.vet,listaEst.quantidade);
+    selectionSort(listaEst.vet,listaEst.quantidade);
     return 0;
 }
